52f2304bae7ea462c6b53ba5cc07ffab270d7b41
Alla Massorova <zebrasteel@gmail.com>
2020-04-02 16:10:59 +0200

    select results from DB

--- app.rb
+++ app.rb
@@ -23,6 +23,7 @@ configure do
 end
 get '/' do
 	#erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	@results = @db.execute 'select * from Posts order by id desc'
 erb :index
 end
 # обработка get-запрса /new => браузер получает страницу с сервера
