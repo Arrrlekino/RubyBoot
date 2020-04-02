255c551431650c387f617c7a810efd272883de99
Alla Massorova <zebrasteel@gmail.com>
2020-04-02 14:55:08 +0200

    added comments

--- app.rb
+++ app.rb
@@ -11,8 +11,12 @@ end
 before do
 	init_db
 end	
+# configure ввызывается каждый раз при конфигурации приложения:
+# инициализация БД когда меняем код пр-мы или перезагрузилась страница
 configure do
+	# инициализация БД
 	init_db
+	# создает таблицу если такой не существует
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts
 	(id INTEGER PRIMARY KEY AUTOINCREMENT, created_date DATE, content TEXT
 	)'
@@ -20,13 +24,16 @@ end
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+# обработка get-запрса /new => браузер получает страницу с сервера
 get '/new' do
 erb	:new
 end	
 #get '/new' do
  # "Hello World"
 #end
+# обработка post-запрса /new => браузер отправляет страницу на сервер
 post '/new' do
+	# получаем переменную из post-запрса
 	content = params[:content]
 	erb "you typed #{content}"
 end
